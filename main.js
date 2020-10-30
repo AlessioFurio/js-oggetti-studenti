7394c89dcc6f5d66b5b883f30f8db5965aba789c
Furio Alessio <alefurio@hotmail.it>
2020-10-30 17:19:18 +0100

    oggetto studente + stampa delle prorieta' nome e cognome

--- main.js
+++ main.js
@@ -12,9 +12,42 @@ var studente = {
     'cognome': 'Furio',
     'eta': 27
 }
+console.log(studente);
+
 // Attraverso un ciclo for-in stampare a schermo tutte le sue proprietà e il relativo valore
 for (var key in studente) {
     console.log(key + ': ' + studente[key]);
 }
+
+
+// creare un array di oggetti "studente".
+var classe = [
+    {
+        'nome': 'Mario',
+        'cognome': 'Rossi',
+        'eta': 10
+    },
+    {
+        'nome': 'Luigi',
+        'cognome': 'Bianchi',
+        'eta': 11
+    },
+    {
+        'nome': 'Franco',
+        'cognome': 'Neri',
+        'eta': 9
+    }
+];
+
+// Ciclare su tutti gli studenti e stampare per ciascuno il nome e il cognome
+for (var i = 0; i < classe.length; i++) {
+
+    for (var key in classe[i]) {
+    }
+    console.log(classe[i]['nome'] + ' ' + classe[i]['cognome']);
+
+}
+
+// tramite 3 prompt, chiedere all'utente il nome, il cognome e l'età di un nuovo studente. Creare con questi dati un nuovo oggetto e inserirlo nell'array del punto precedente.
 
 });
